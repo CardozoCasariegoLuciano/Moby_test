fc5a5df79db7daaf970c7e06447c8ccd43136df2
Luciano Cardozo Casariego <lcardozo@mobydigital.com>
2022-08-22 18:40:59 -0300

    refactor edit user form

--- src/app/shared/components/edit-user-form/edit-user-form.component.ts
+++ src/app/shared/components/edit-user-form/edit-user-form.component.ts
@@ -13,7 +13,7 @@ export class EditUserFormComponent implements OnInit {
   showExtraField: boolean = false;
   areGeoEquals: boolean = true;
   @Input() userData: Iauth | undefined;
-  @Output() oncloseModal: EventEmitter<boolean> = new EventEmitter();  
+  @Output() oncloseModal: EventEmitter<boolean> = new EventEmitter();
 
   constructor(private fb: FormBuilder, private authService: AuthService) {}
 
@@ -24,69 +24,44 @@ export class EditUserFormComponent implements OnInit {
 
   private initForm() {
     this.editUserForm = this.fb.group({
-      name: [null, [Validators.required]],
-      username: [null, [Validators.required]],
-      password: [null],
-      newPasswords: [null],
-      repeatNewPassword: [null],
-      phone: [null],
-      webSite: [null],
-      address: this.fb.group({
-        street: [null],
-        suite: [null],
-        city: [null],
-        zipcode: [null],
-        geo: this.fb.group({
-          lat: [null],
-          lng: [null],
-        }),
-      }),
-      company: this.fb.group({
-        name: [null],
-        catchPhrase: [null],
-        bs: [null],
-      }),
+      name: ['', [Validators.required]],
+      username: ['', [Validators.required]],
+      password: [''],
+      newPasswords: [''],
+      repeatNewPassword: [''],
+      phone: [''],
+      webSite: [''],
+      street: [''],
+      suite: [''],
+      city: [''],
+      zipcode: [''],
+      lat: [''],
+      lng: [''],
+      companyName: [''],
+      catchPhrase: [''],
+      bs: [''],
     });
   }
 
   setDefaultValues() {
-    this.editUserForm.controls['name'].reset(this.userData!.name);
-    this.editUserForm.controls['username'].reset(this.userData!.username);
+    this.editUserForm.get('name')?.reset(this.userData!.name);
+    this.editUserForm.get('username')?.reset(this.userData!.username);
     this.editUserForm.get('phone')?.reset(this.userData!.phone);
     this.editUserForm.get('webSite')?.reset(this.userData!.website);
-
+    this.editUserForm.get('street')?.reset(this.userData!.address?.street);
+    this.editUserForm.get('suite')?.reset(this.userData!.address?.suite);
+    this.editUserForm.get('zipcode')?.reset(this.userData!.address?.zipcode);
+    this.editUserForm.get('city')?.reset(this.userData!.address?.city);
+    this.editUserForm.get('lat')?.reset(this.userData!.address?.geo?.lat);
+    this.editUserForm.get('lng')?.reset(this.userData!.address?.geo?.lng);
+    this.editUserForm.get('companyName')?.reset(this.userData!.company?.name);
     this.editUserForm
-      .get('address.street')
-      ?.reset(this.userData!.address?.street);
-
-    this.editUserForm
-      .get('address.suite')
-      ?.reset(this.userData!.address?.suite);
-
-    this.editUserForm
-      .get('address.zipcode')
-      ?.reset(this.userData!.address?.zipcode);
-
-    this.editUserForm.get('address.city')?.reset(this.userData!.address?.city);
-
-    this.editUserForm
-      .get('address.geo.lat')
-      ?.reset(this.userData!.address?.geo?.lat);
-
-    this.editUserForm
-      .get('address.geo.lng')
-      ?.reset(this.userData!.address?.geo?.lng);
-
-    this.editUserForm.get('company.name')?.reset(this.userData!.company?.name);
-
-    this.editUserForm
-      .get('company.catchPhrase')
+      .get('catchPhrase')
       ?.reset(this.userData!.company?.catchPhrase);
-
-    this.editUserForm.get('company.bs')?.reset(this.userData!.company?.bs);
+    this.editUserForm.get('bs')?.reset(this.userData!.company?.bs);
   }
 
-  editData() {    
+  editData() {
     if (this.editUserForm.invalid) return;
     if (!this.arePasswordsOk()) return;
     if (!this.isGeoOk()) return;
@@ -95,9 +70,30 @@ export class EditUserFormComponent implements OnInit {
       id: this.userData!.id!,
       name: this.editUserForm.get('name')?.value,
       username: this.editUserForm.get('username')?.value,
+      phone: this.editUserForm.get('phone')?.value,
+      website: this.editUserForm.get('webSite')?.value,
+      address: {
+        street: this.editUserForm.get('street')?.value,
+        suite: this.editUserForm.get('suite')?.value,
+        city: this.editUserForm.get('city')?.value,
+        zipcode: this.editUserForm.get('zipcode')?.value,
+        geo: {
+          lat: this.editUserForm.get('lat')?.value,
+          lng: this.editUserForm.get('lng')?.value,
+        },
+      },
+      company: {
+        name: this.editUserForm.get('companyName')?.value,
+        catchPhrase: this.editUserForm.get('catchPhrase')?.value,
+        bs: this.editUserForm.get('bs')?.value,
+      },
     };
 
-    data = this.addFields(data);
+    const password = this.editUserForm.get('newPasswords')?.value;
+
+    if (password) {
+      data.password = password;
+    }
 
     this.authService.editUser(data).subscribe();
     this.closeModal();
@@ -109,21 +105,17 @@ export class EditUserFormComponent implements OnInit {
       this.editUserForm.controls[name].touched
     );
   }
+
   arePasswordsOk() {
     const actualPass = this.userData!.password;
     const pass = this.editUserForm.controls['password'];
     const newPass = this.editUserForm.controls['newPasswords'];
     const repeateNewPass = this.editUserForm.controls['repeatNewPassword'];
 
-    if (pass.value === '') {
-      pass.setValue(null);
-      return true;
-    }
-
     if (
-      pass.value === null &&
-      newPass.value === null &&
-      repeateNewPass.value === null
+      pass.value === '' &&
+      newPass.value === '' &&
+      repeateNewPass.value === ''
     ) {
       return true;
     }
@@ -144,11 +136,11 @@ export class EditUserFormComponent implements OnInit {
   }
 
   isGeoOk() {
-    const lat = this.editUserForm.get('address.geo.lat')?.value;
-    const lng = this.editUserForm.get('address.geo.lng')?.value;
+    const lat = this.editUserForm.get('lat')?.value;
+    const lng = this.editUserForm.get('lng')?.value;
 
-    const onlyLat = lat != null && lng == null;
-    const onlyLng = lng != null && lat == null;
+    const onlyLat = lat != "" && lng == "";
+    const onlyLng = lng != "" && lat == "";
 
     if (onlyLng) {
       this.areGeoEquals = false;
@@ -162,97 +154,6 @@ export class EditUserFormComponent implements OnInit {
 
     this.areGeoEquals = true;
     return true;
-  }
-
-  addFields(data: Iauth): Iauth {
-    const pass = this.editUserForm.controls['newPasswords'].value;
-    const phone = this.editUserForm.controls['phone'].value;
-    const webSite = this.editUserForm.controls['webSite'].value;
-
-    const street = this.editUserForm.get('address.street')?.value;
-    const suite = this.editUserForm.get('address.suite')?.value;
-    const city = this.editUserForm.get('address.city')?.value;
-    const zipcode = this.editUserForm.get('address.zipcode')?.value;
-
-    const lat = this.editUserForm.get('address.geo.lat')?.value;
-    const lng = this.editUserForm.get('address.geo.lng')?.value;
-
-    const companyName = this.editUserForm.get('company.name')?.value;
-    const catchPhrase = this.editUserForm.get('company.catchPhrase')?.value;
-    const bs = this.editUserForm.get('company.bs')?.value;
-
-    if (pass != null) {
-      data.password = pass;
-    }
-
-    if (phone != null) {
-      data.phone = phone;
-    }
-
-    if (webSite != null) {
-      data.website = webSite;
-    }
-
-    if (street != null) {
-      if (!data.address) {
-        data.address = {};
-      }
-      data.address.street = street;
-    }
-
-    if (suite != null) {
-      if (!data.address) {
-        data.address = {};
-      }
-      data.address.suite = suite;
-    }
-
-    if (city != null) {
-      if (!data.address) {
-        data.address = {};
-      }
-      data.address.city = city;
-    }
-
-    if (zipcode != null) {
-      if (!data.address) {
-        data.address = {};
-      }
-      data.address.zipcode = zipcode;
-    }
-
-    if (lat != null && lng != null) {
-      if (!data.address) {
-        data.address = {};
-      }
-      if (!data.address.geo) {
-        data.address.geo = {};
-      }
-      data.address.geo.lat = lat;
-      data.address.geo.lng = lng;
-    }
-
-    if (companyName != null) {
-      if (!data.company) {
-        data.company = {};
-      }
-      data.company.name = companyName;
-    }
-
-    if (catchPhrase != null) {
-      if (!data.company) {
-        data.company = {};
-      }
-      data.company.catchPhrase = catchPhrase;
-    }
-
-    if (bs != null) {
-      if (!data.company) {
-        data.company = {};
-      }
-      data.company.bs = bs;
-    }
-    return data;
   }
 
   displayMoreFields() {
